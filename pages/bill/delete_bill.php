60df22511093d36dc24c802add49e3c04485e64e
thammeeeeerrrttt-collab <thammeeeeerrrttt@gmail.com>
2026-06-19 02:22:39 +0300

    Update delete_bill.php

--- pages/bill/delete_bill.php
+++ pages/bill/delete_bill.php
@@ -11,7 +11,7 @@ $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 $periodID = isset($_GET['period_id']) ? intval($_GET['period_id']) : '';
 
 if($id){
-    $stmt = $conn->prepare("DELETE FROM Bill WHERE BillID = ?");
+    $stmt = $conn->prepare("DELETE FROM bill WHERE BillID = ?");
     $stmt->bind_param("i", $id);
     $stmt->execute();
     $stmt->close();
